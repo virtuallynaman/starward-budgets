4048811f903bb4f590331aff90aba76ee300b3b8
virtuallynaman <jhanaman786@gmail.com>
2025-05-17 22:08:24 +0530

    Add toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,60 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Dashboard, { dashboardLoader } from "./pages/Dashboard"
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
 import Error from "./pages/Error"
-import MainLayout from "./layouts/MainLayout"
+import MainLayout from "./layouts/RootLayout"
+import ExpensesPage from "./pages/ExpensesPage";
+import BudgetPage from "./pages/BudgetPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PublicRoute from "./components/PublicRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { BudgetProvider } from "./contexts/BudgetContext";
 
 const router = createBrowserRouter([
   {
+    path: "/login",
+    element: <PublicRoute><Login /></PublicRoute>,
+    errorElement: <Error />
+  },
+  {
+    path: "/signup",
+    element: <PublicRoute><Signup /></PublicRoute>,
+    errorElement: <Error />
+  },
+  {
     path: "/",
-    element: <MainLayout />,
+    element: <ProtectedRoute><MainLayout /></ProtectedRoute>,
     errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Dashboard />,
-        loader: dashboardLoader,
+        errorElement: <Error />
+      },
+      {
+        path: "budget/:id",
+        element: <BudgetPage />,
+        errorElement: <Error />
+      },
+      {
+        path: "expenses",
+        element: <ExpensesPage />,
         errorElement: <Error />
       }
     ]
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" />
   }
 ]);
 
 function App() {
 
   return (
-    <>
+    <BudgetProvider>
       <RouterProvider router={router} />
-    </>
+    </BudgetProvider>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'sonner'
+import { AuthProvider } from './contexts/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Toaster for toast notifications */}
+    <Toaster
+      toastOptions={{
+        style: {
+          minWidth: '200',
+          maxWidth: '400px',
+          width: 'fit-content',
+          wordBreak: 'break-word',
+          fontSize: '16px',
+          padding: '10px 15px',
+          textTransform: 'capitalize'
+        },
+      }}
+      visibleToasts={2}
+    />
+
+    <AuthProvider>
+        <App />
+    </AuthProvider>
   </StrictMode>,
 )
