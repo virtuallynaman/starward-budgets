64f1e3be87ff032dea150eab9e7a6caee989cc2e
virtuallynaman <jhanaman786@gmail.com>
2025-05-17 22:06:46 +0530

    Add auth middleware

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const requireAuth = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(400).json({ error: "Not authorized" });
+    }
+    const token = authorization.split(" ")[1];
+
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+
+        req.user = payload;
+        next();
+    } catch (err) {
+        console.error("Error verifying the token", err);
+        return res.status(401).json({ error: "Request is not authorized. Try logging in." });
+    }
+}
+
+export default requireAuth
