860cc9291416bf2cf3b665968fbf15d876fe884c
virtuallynaman <jhanaman786@gmail.com>
2025-05-18 22:08:11 +0530

    Add footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,13 +2,15 @@ import { FaGithub, FaUserAstronaut } from 'react-icons/fa'
 
 function Footer() {
     return (
-        <div className='footer-content'>
-            <div className='footer-links'>
-                <a href="https://github.com/virtuallynaman/starward-budgets" target="_blank" rel="noopener noreferrer" ><FaGithub /> Github</a>
-                <a href="" target="_blank" rel="noopener noreferrer" ><FaUserAstronaut /> Portfolio</a>
+        <footer>
+            <div className='footer-content'>
+                <div className='footer-links'>
+                    <a href="https://github.com/virtuallynaman/starward-budgets" target="_blank" rel="noopener noreferrer" ><FaGithub /> Github</a>
+                    <a href="" target="_blank" rel="noopener noreferrer" ><FaUserAstronaut /> Portfolio</a>
+                </div>
+                <p className='copyright'>&copy; {new Date().getFullYear()} Starward Budgets</p>
             </div>
-            <p className='copyright'>&copy; {new Date().getFullYear()} Starward Budgets</p>
-        </div>
+        </footer>
     )
 }
 
---
+++ frontend/src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <h2 className='error'>Something went wrong : ( </h2>
+  )
+}
+
+export default Error
