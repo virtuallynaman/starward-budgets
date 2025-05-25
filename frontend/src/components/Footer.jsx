ef7d936ac56ea3b94f054d548f4d56f34d3a9462
virtuallynaman <jhanaman786@gmail.com>
2025-05-25 22:57:43 +0530

    Update links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <div className='footer-content'>
                 <div className='footer-links'>
                     <a href="https://github.com/virtuallynaman/starward-budgets" target="_blank" rel="noopener noreferrer" ><FaGithub /> Github</a>
-                    <a href="" target="_blank" rel="noopener noreferrer" ><FaUserAstronaut /> Portfolio</a>
+                    <a href="https://namanjha.vercel.app" target="_blank" rel="noopener noreferrer" ><FaUserAstronaut /> Portfolio</a>
                 </div>
                 <p className='copyright'>&copy; {new Date().getFullYear()} Starward Budgets</p>
             </div>
